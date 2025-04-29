923aee8694e1cafeb209a20c4a63b87cfa15d809
dreamenson <dreamensonfb@gmail.com>
2025-04-29 17:29:57 +0200

    main time duration

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
+import random.RandomCreator;
 import simulation.MySimulation;
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
+        long startTime = System.currentTimeMillis();
+        RandomCreator.setSeed(42);
+        for (int i = 0; i < 5; i++) {
 //            System.out.println();
-            MySimulation sim = new MySimulation(5,5,38,58);
-            sim.simulate(100);
+            MySimulation sim = new MySimulation(6,5,38,57);
+            sim.simulate(1000);
         }
+        System.out.println("duration: " + (System.currentTimeMillis() - startTime));
     }
 }
